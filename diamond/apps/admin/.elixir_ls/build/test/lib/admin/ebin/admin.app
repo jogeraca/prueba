70dbbdaea8b5cb119b44f6948eb3acb343283f36
German Ramirez <jogeraca@gmail.com>
2018-05-02 19:13:40 -0500

    WIP

---
+++ diamond/apps/admin/.elixir_ls/build/test/lib/admin/ebin/admin.app
@@ -0,0 +1,8 @@
+{application,admin,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"admin"},
+              {modules,['Elixir.Admin','Elixir.Admin.Application']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {extra_applications,[logger]},
+              {mod,{'Elixir.Admin.Application',[]}}]}.
--- diamond/mix.exs
+++ diamond/mix.exs
@@ -3,9 +3,11 @@ defmodule Diamond.MixProject do
 
   def project do
     [
+      elixir: "~> 1.6.3",
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -16,5 +18,14 @@ defmodule Diamond.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     []
+  end
+
+  defp aliases do
+    [
+      "ecto.clean": ["ecto.drop", "ecto.create", "ecto.migrate"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create", "ecto.migrate", "test"]
+    ]
   end
 end
